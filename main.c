03e6e9d05b4de1fa811b95a4b8c9c5454c62d1af
rfautier <rfautier@student.42.fr>
2018-01-18 18:26:33 +0100

    add main.c

--- main.c
+++ main.c
@@ -12,39 +12,45 @@
 
 #include "main.h"
 
+void loop(char **tab, char **environ)
+{
+	if (!tab[0])
+		;
+	else if (ft_strcmp(tab[0], "exit") == 0)
+		exit(0);
+	else if (ft_strcmp(tab[0], "clear") == 0)
+		ft_putstr("\033[H\033[2J");
+	else if (ft_strcmp(tab[0], "cd") == 0)
+		environ = check_cd(tab, environ);
+	else if (ft_strcmp(tab[0], "env") == 0)
+		check_env(tab, environ);
+	else if (ft_strcmp(tab[0], "setenv") == 0)
+		environ = check_setenv(tab, environ);
+	else if (ft_strcmp(tab[0], "unsetenv") == 0)
+		environ = check_unsetenv(tab, environ);
+	else if (ft_strcmp(tab[0], "echo") == 0)
+		check_echo(tab, environ);
+	else if (lunchprocessus(tab, environ))
+		;
+	else
+		ft_putstr("Command not found\n");
+}
+
 int main(int argc, char **argv, char **env)
 {
-    char **tab;
-    extern char **environ;
+	char **tab;
+	extern char **environ;
 
-    argc = 0;
-    argv = NULL;
-    env = NULL; //Rajouter un main
-    ft_putstr("\033[H\033[2J");
-    while (1)
-    {
-        tab = get_cmd();
-        signal(SIGINT, sign);
-        if (!tab[0])
-            ;
-        else if (ft_strcmp(tab[0], "exit") == 0)
-            exit(0);
-        else if (ft_strcmp(tab[0], "clear") == 0)
-            ft_putstr("\033[H\033[2J");
-        else if (ft_strcmp(tab[0], "cd") == 0)
-            environ = check_cd(tab, environ);
-        else if (ft_strcmp(tab[0], "env") == 0)
-            check_env(tab, environ);
-        else if (ft_strcmp(tab[0], "setenv") == 0)
-            environ = check_setenv(tab, environ);
-        else if (ft_strcmp(tab[0], "unsetenv") == 0)
-            environ = check_unsetenv(tab, environ);
-        else if (ft_strcmp(tab[0], "echo") == 0)
-            check_echo(tab, environ);
-        else if (lunchprocessus(tab, environ))
-            ;
-        else
-            ft_putstr("Command not found\n");
-    }
-    return (0);
+	argc = 0;
+	argv = NULL;
+	env = NULL;
+
+	ft_putstr("\033[H\033[2J");
+	while (1)
+	{
+		signal(SIGINT, sign);
+		tab = get_cmd();
+		loop(tab, environ);
+	}
+	return (0);
 }
